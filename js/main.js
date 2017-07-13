4435ff85cb11aceb2f92deef0e2c899ad2174658
hexenq <hexenq@gmail.com>
2017-07-13 19:00:02 +0800

    update js

--- js/main.js
+++ js/main.js
@@ -60,6 +60,14 @@ on('load',
             $body.className = $body.className.replace(/\bis-loading\b/, 'is-playing');
             setTimeout(function () {
                 $body.className = $body.className.replace(/\bis-playing\b/, 'is-ready');
+
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+                ga('create', 'UA-71952677-1', 'auto');
+                ga('send', 'pageview');
             },
                 1000);
         },
